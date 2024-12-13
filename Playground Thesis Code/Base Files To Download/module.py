0ebd81e738eb8aa3fb02802b94e2303e873e3fe6
Sophie Hankin <sophie.hankin@tufts.edu>
2024-12-13 16:24:25 -0500

    Reorganizing

---
+++ Playground Thesis Code/Base Files To Download/module.py
@@ -0,0 +1,125 @@
+import time
+import struct
+from networking import Networking
+from led_sequence import LEDSequencer
+import ledmatrix
+import ssd1306
+import sensors
+from machine import Pin, SoftI2C, PWM, ADC
+
+#Define module class
+class Module:
+    def __init__(self, name = 'Lion'): #change to module animal
+        self.status = 'searching'
+        self.board_mac = None
+        self.count = 0
+        self.sequence = []
+        self.player_sequence = []
+        self.screen_message = None
+        self.received_message = None
+        self.motor = Pin(2, Pin.OUT)
+        
+        #Color dictionary
+        self.c = {
+        1:'red',
+        2:'orange',
+        3:'yellow',
+        4:'green',
+        5:'cyan',
+        6:'blue',
+        7:'purple',
+        8:'pink',
+        }
+        
+        #Initialize ESPNOW
+        self.networking = Networking(True, False) #First bool is for network info messages, second for network debug messages
+        self.broadcast_mac = b'\xff\xff\xff\xff\xff\xff'
+        self.networking.aen.add_peer(self.broadcast_mac, "All")
+        self.networking.name = name
+        
+        #oled screen
+        self.i2c = SoftI2C(scl = Pin(7), sda = Pin(6))
+        self.oled = ssd1306.SSD1306_I2C(128, 64, self.i2c)
+        self.oled.fill(0)
+
+        #Led Matrix setup
+        self.matrix = ledmatrix.LEDMATRIX(self.i2c)
+        self.sequencer = LEDSequencer(self.matrix)
+        self.buffer = self.sequencer.buffer
+        self.clear_display()
+        
+    def clear_display(self):
+        self.sequencer.clear_display()
+        
+    def vibrate(self, delay = 0.2):
+        self.motor.value(1)
+        time.sleep(delay)
+        self.motor.value(0)
+    
+    def send(self, mac, message):
+        self.networking.aen.send(mac, message)
+    
+    def screen_display(self, message):
+        self.screen_message = message
+        if message == None:
+            self.oled.fill(0)
+        else:
+            self.oled.text(message,0,0)
+        self.oled.show()
+    
+    def display_sequence(self,sequence):
+        self.sequence = sequence
+        for i in range(0,len(sequence)):
+            num = sequence[i]
+            color = self.c[num]
+            self.sequencer.display_color_pixel(color,7-i,7)
+        self.sequencer.display_number(sequence[0],self.c[sequence[0]])
+        
+    def display_player_sequence(self):
+        for i in range(0,len(self.player_sequence)):
+            num = self.player_sequence[i]
+            color = self.c[num]
+            self.sequencer.display_color_pixel(color,7-i,6)
+    
+    def display_wrong(self):
+        for i in range(0,8):
+            self.sequencer.display_color_pixel('red',7-i,6)
+        time.sleep(0.75)
+        for i in range(0,8):
+            self.sequencer.display_color_pixel('black',7-i,6)
+    
+    def display_correct(self):
+        buffer = [0x52]*64
+        self.matrix.display_frames(buffer, 1000,1,1)
+        
+    def add_to_sequence(self,num):
+        self.sequence.append(num)
+        color = self.c[num]
+        self.sequencer.display_number(num,color)
+        self.sequencer.display_color_pixel(color,7-(len(self.sequence)-1),7)
+    
+    def check_buffer(self,num):
+        index = len(self.player_sequence)
+        color = self.c[num]
+        print(7-(len(self.player_sequence)-1))
+        self.sequencer.display_color_pixel(color,7-index,6)
+        if num == self.sequence[index]:
+            self.player_sequence.append(num)
+            if (index + 1) < len(self.sequence):
+                self.sequencer.display_number(self.sequence[index+1],self.c[self.sequence[index+1]])
+            else:
+                self.display_correct()
+            return True
+        else:
+            self.vibrate()
+            self.display_wrong()
+            self.display_player_sequence()
+            return False
+        
+    def reset(self):
+        self.status = 'searching'
+        self.board_mac = None
+        self.count = 0
+        self.sequence = []
+        self.player_sequence = []
+        self.clear_display()
---
+++ Playground Thesis Code/overall_module_main.py
@@ -0,0 +1,97 @@
+#Imports
+import time
+import struct
+from networking import Networking
+from led_sequence import LEDSequencer
+import ledmatrix
+import ssd1306
+import sensors
+from module import Module
+from machine import Pin, SoftI2C, PWM, ADC
+
+module = Module()
+num = None
+last_num = None
+checked = False
+
+def receive():
+    global num, checked
+    for mac, message, rtime in module.networking.aen.return_messages():
+        module.received_message = message
+        
+        if message == 'coder':
+            switch_select.irq(trigger=Pin.IRQ_FALLING, handler=None)
+            module.board_mac = mac
+            module.screen_display('Accept Coder?')
+            start_time = time.time()
+            while time.time() - start_time < 5:  # Keep active for 3 seconds
+                if switch_select.value() == 0:
+                    module.status = 'coder'
+                    module.send(module.board_mac, 'coder')
+                    time.sleep(0.5)
+                    switch_select.irq(trigger=Pin.IRQ_FALLING, handler=select)
+                    module.clear_display()
+                    module.sequence = []
+                    break
+                time.sleep(0.05)
+            module.screen_display(None)
+            
+        elif isinstance(message, list): #received a sequence
+            switch_select.irq(trigger=Pin.IRQ_FALLING, handler=None)
+            module.board_mac = mac
+            module.screen_display('Accept Player?')
+            start_time = time.time()
+            while time.time() - start_time < 5:  # Keep active for 3 seconds
+                if switch_select.value() == 0:
+                    module.status = 'player'
+                    module.send(module.board_mac, 'player')
+                    time.sleep(1)
+                    module.clear_display()
+                    module.player_sequence = []
+                    module.display_sequence(message)
+                    break
+                time.sleep(0.05)
+            module.screen_display(None)
+            
+        else: #received a number
+            num = int(message)
+            checked = False
+            
+def select(pin):
+    global num, last_num
+    if module.status == 'coder':
+        module.networking.aen.ping(module.board_mac)
+        rssi = module.networking.aen.rssi()[module.board_mac][0]
+        print(rssi)
+        if rssi > -25:
+            module.send(module.board_mac, module.sequence)
+        #if within trash range:
+            #module.reset()
+            #num = None
+            #last_num = None
+        #else:
+            #display move closer icon
+            
+#Callbacks
+switch_select = Pin(9, Pin.IN, Pin.PULL_UP)
+module.networking.aen.irq(receive)
+
+while True:
+    if module.status == 'coder' and module.count < 8:
+        if num != last_num and num != None:
+            module.add_to_sequence(num)
+            module.count += 1
+            last_num = num
+    elif module.status != 'searching':
+        if not checked and num != None:
+            matches = module.check_buffer(num)
+            checked = True
+            if matches:
+                module.send(module.board_mac, module.player_sequence)
+
+    time.sleep(2)
+                
+            
+            
+            
+            
