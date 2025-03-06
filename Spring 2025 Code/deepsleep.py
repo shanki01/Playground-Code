78b35299d8fe976906bccd32574db0bcc24f07cc
aengusk <116749459+aengusk@users.noreply.github.com>
2025-03-06 16:57:33 -0500

    Clean up deepsleep.py

--- Spring 2025 Code/deepsleep.py
+++ Spring 2025 Code/deepsleep.py
@@ -1,9 +1,12 @@
-from machine import Pin,lightsleep,SLEEP
-from time import sleep_ms
-import machine
 import time
+from machine import Pin, deepsleep # type: ignore 
+import esp32 # type: ignore 
 
-led = Pin(3, Pin.OUT)
+led = Pin(0, Pin.OUT)
+wake_up_button = Pin(2, Pin.IN, Pin.PULL_UP)
+
+# esp32.WAKEUP_ANY_HIGH or esp32.WAKEUP_ALL_LOW
+esp32.wake_on_ext1(pins = (wake_up_button,), level = esp32.WAKEUP_ALL_LOW)
 
 def blink_led(times, delay_time):
     for _ in range(times):
@@ -11,18 +14,11 @@ def blink_led(times, delay_time):
         time.sleep(delay_time)
         led.value(0)
         time.sleep(delay_time)
+
 blink_led(3, 0.4)
-import esp32
 
+while wake_up_button.value(): 
+    print('waiting for button press, and then will go to sleep until button is released')
+    time.sleep(1)
 
-wake_up = Pin(2, Pin.IN, Pin.PULL_UP)
-
-#sp32.WAKEUP_ANY_HIGH or esp32.WAKEUP_ALL_LOW
-esp32.wake_on_ext1(pins = (wake_up,), level = esp32.WAKEUP_ALL_LOW)
-
-
-print('Im awake now. Going to sleep in 5 seconds ...')
-#time.sleep(5)
-print('Going to sleep now ..')
-
-machine.deepsleep()
+deepsleep()
